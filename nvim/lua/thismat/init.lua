0236919234d324e89c4ccde7a489ae606c24a49c
thismat <m@thismat.com>
2022-06-17 09:52:16 -0500

    Fixed swap file getting committed

--- nvim/lua/thismat/init.lua
+++ nvim/lua/thismat/init.lua
@@ -1,1 +1,8 @@
-require('options')
+local submodules = {
+	'options'
+}
+
+-- TODO: Is there a better way to do this?
+for i, submodule in pairs(submodules) do
+	require('thismat.' .. submodule)
+end
