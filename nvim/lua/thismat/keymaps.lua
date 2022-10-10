73410f8c2b4df6598cde5fea017996cfe10d1bce
thismat <thismat@gmail.com>
2022-10-10 00:05:04 -0500

    Added lua-dev for better lua help

--- nvim/lua/thismat/keymaps.lua
+++ nvim/lua/thismat/keymaps.lua
@@ -1,8 +1,5 @@
 local opts = { noremap = true }
 
--- TODO: Move this or leave it here?
--- vim.api.nvim_set_keymap("n", "<A-1>", ":NvimTreeToggle<CR>", opts)
-
 -- Diagnotics keymaps
 vim.keymap.set("n", "gh", vim.diagnostic.open_float, opts)
 vim.keymap.set("n", "ne", vim.diagnostic.goto_next, opts)
--- nvim/lua/thismat/plugins.lua
+++ nvim/lua/thismat/plugins.lua
@@ -1,63 +1,63 @@
 -- List of plugins and configuration
 
 -- If windows specific setup is needed
-local WINDOWS = vim.fn.has('win64') or vim.fn.has('win32')
+local WINDOWS = vim.fn.has("win64") or vim.fn.has("win32")
 
 local plugins = {
   -- Packer
   {
-    'wbthomason/packer.nvim'
+    "wbthomason/packer.nvim"
   },
 
   -- Treesitter
   {
-    'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'
+    "nvim-treesitter/nvim-treesitter", run = ":TSUpdate"
   },
 
   -- Colorschemes
   {
-    'folke/tokyonight.nvim'
+    "folke/tokyonight.nvim"
   },
 
   -- LSP and Completion
   {
-    'neovim/nvim-lspconfig'
+    "neovim/nvim-lspconfig"
   },
   {
-    'hrsh7th/cmp-nvim-lsp'
+    "hrsh7th/cmp-nvim-lsp"
   },
   {
-    'hrsh7th/cmp-buffer'
+    "hrsh7th/cmp-buffer"
   },
   {
-    'hrsh7th/cmp-path'
+    "hrsh7th/cmp-path"
   },
   {
-    'hrsh7th/cmp-cmdline'
+    "hrsh7th/cmp-cmdline"
   },
   {
-    'hrsh7th/nvim-cmp'
+    "hrsh7th/nvim-cmp"
   },
   {
-    'onsails/lspkind.nvim'
+    "onsails/lspkind.nvim"
   },
 
   -- Snippets
   {
-    'hrsh7th/cmp-vsnip'
+    "hrsh7th/cmp-vsnip"
   },
   {
-    'hrsh7th/vim-vsnip'
+    "hrsh7th/vim-vsnip"
   },
 
   -- Icons
   {
-    'kyazdani42/nvim-web-devicons'
+    "kyazdani42/nvim-web-devicons"
   },
 
   -- File browser
   {
-    'kyazdani42/nvim-tree.lua',
+    "kyazdani42/nvim-tree.lua",
     config = function()
       require("nvim-tree").setup {}
       vim.api.nvim_set_keymap("n", "<A-1>", ":NvimTreeToggle<CR>", {})
@@ -66,7 +66,7 @@ local plugins = {
 
   -- JSON Schema Store
   {
-    'b0o/schemastore.nvim'
+    "b0o/schemastore.nvim"
   },
 
   -- Trouble LSP diagnostics
@@ -78,6 +78,30 @@ local plugins = {
         use_diagnostic_signs = true
       }
       vim.api.nvim_set_keymap("n", "<A-4>", ":TroubleToggle<CR>", {})
+    end
+  },
+
+  -- Bufferline
+  {
+    "akinsho/bufferline.nvim",
+    config = function()
+      require("bufferline").setup {
+        options = {
+          diagnostics = "nvim_lsp",
+          diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local icon = level:match("error") and " " or " "
+            return " " .. icon .. count
+          end
+      },
+      }
+    end
+  },
+
+  -- LUA Dev (Neovim Specific)
+  {
+    "folke/lua-dev.nvim",
+    config = function()
+      require("lua-dev").setup {}
     end
   }
 }
