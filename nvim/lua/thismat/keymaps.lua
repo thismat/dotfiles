fff6fe07a3c55b7f9da54c26f4f3332879260d71
thismat <thismat@gmail.com>
2022-10-17 20:12:40 -0500

    Added buffer navigation with alt-h and alt-l

--- nvim/lua/thismat/keymaps.lua
+++ nvim/lua/thismat/keymaps.lua
@@ -4,3 +4,7 @@ local opts = { noremap = true }
 vim.keymap.set("n", "gh", vim.diagnostic.open_float, opts)
 vim.keymap.set("n", "ne", vim.diagnostic.goto_next, opts)
 vim.keymap.set("n", "nE", vim.diagnostic.goto_prev, opts)
+
+-- Buffer navigation
+vim.keymap.set("n", "<A-h>", ":bprevious<CR>", opts);
+vim.keymap.set("n", "<A-l>", ":bnext<CR>", opts);
