6081cf4aa55cf4e589ca7d0aef3333d429f1cb7d
thismat <thismat@gmail.com>
2022-10-18 17:25:04 -0500

    Added c-bs support for deleting words

--- nvim/lua/thismat/keymaps.lua
+++ nvim/lua/thismat/keymaps.lua
@@ -8,3 +8,7 @@ vim.keymap.set("n", "nE", vim.diagnostic.goto_prev, opts)
 -- Buffer navigation
 vim.keymap.set("n", "<A-h>", ":bprevious<CR>", opts);
 vim.keymap.set("n", "<A-l>", ":bnext<CR>", opts);
+
+-- Insert Mode Specific
+vim.keymap.set("i", "<C-BS>", "<C-w>", opts);
+vim.keymap.set("i", "<C-h>", "<C-w>", opts);
