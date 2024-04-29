29ab238f7cedd347d3abf011bbade480a1927e92
thismat <thismat@gmail.com>
2024-04-29 09:02:32 -0500

    updated windows with tailwind installer

--- windows/install-apps.ps1
+++ windows/install-apps.ps1
@@ -1,29 +1,38 @@
-function Test-Installed {
-    param (
-        [Parameter(Mandatory)]
-        [string]$appName
-    )
+function Test-Installed
+{
+  param (
+    [Parameter(Mandatory)]
+    [string]$appName
+  )
 
-    if (Get-Command $appName -ErrorAction SilentlyContinue) {
-        return $true;
-    } else {
-        return $false;
-    }
+  if (Get-Command $appName -ErrorAction SilentlyContinue)
+  {
+    return $true;
+  } else
+  {
+    return $false;
+  }
 
 }
 
 $apps = "JanDeDobbeleer.OhMyPosh",
-        "ajeetdsouza.zoxide"
+"ajeetdsouza.zoxide",
+"TailwindLabs.TailwindCSS"
 
 Write-Output "$apps"
 $progress = 0;
-foreach ($app in $apps) {
-    $installed = Test-Installed $app
-    if ($installed -eq $false) {
-        Write-Output "Installing: $app"
-        & winget install $app -e --source winget
-    }
+foreach ($app in $apps)
+{
+  $installed = Test-Installed $app
+  if ($installed -eq $false)
+  {
+    Write-Output "Installing: $app"
+    & winget install $app -e --source winget
+  }
 
-    $progress++
-    Write-Progress -Activity "Installing Apps" -PercentComplete $progress
+  $progress++
+  Write-Progress -Activity "Installing Apps" -PercentComplete $progress
 }
+
+
+
